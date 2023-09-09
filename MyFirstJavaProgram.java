d631c139ed71f45afdfdb1872151fc8b84c7ca43
GangadiSowmya <144530096+GangadiSowmya@users.noreply.github.com>
2023-09-09 16:47:35 +0530

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+
+public class MyFirstJavaProgram 
+{
+   public static void main(String[] args)
+   {
+	System.out.println("integer" + Integer.SIZE);
+	System.out.println("integer" + Integer.SIZE/8);
+	
+   }
+}
+ 
